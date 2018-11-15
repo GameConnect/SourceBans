61d67a836da3fbd77e99d74a86854c85a78c136b
Erik Minekus <tsunami@tsunami-productions.nl>
2018-11-15 20:53:29 +0100

    Add web profiler

--- web/config/bundles.php
+++ web/config/bundles.php
@@ -7,4 +7,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
